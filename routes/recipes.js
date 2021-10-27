71ee941884975faf76b3a9820225f917dc040e5f
MarcoMueglich <92206527+MarcoMueglich@users.noreply.github.com>
2021-10-27 11:00:48 +0200

    First Try with MongoDB

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,56 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Recipe = require('../models/Recipe');
 
-let recipes = [
-    {
-        recipeID: 242305,
-        title: 'Lasagna',
-        category: 'Main Dish',
-        photoUrl: 'https://photos.bigoven.com/recipe/hero/kellys-lasagne.jpg',
-        ingredients: [
-            'SoySauce',
-            'Vegetable Oil',
-            'Brown Sugar',
-            'Fresh Ginger',
-        ],
-        instructions:
-            'Prepare marinade:\r\n\r\nMix soy sauce, dry sherry, vegetable oil, brown sugar and ginger. Whisk to incorporate. \r\n\r\nAdd to plastic ziptop bag with chicken breasts. Gently press to remove air, and place in a bowl in the fridge for at least 2 hours, up to 6. \r\n\r\nTurn on grill to medium heat. Oil grill. Place chicken breasts on grill, and let cook for 2 minutes. Then turn 90 degrees. Cook for 2 more minutes and flip. Cook for 2 more minutes. Turn 90 degrees. Check for doneness, remove from grill when done. Let rest on cutting board for 2 minutes. Slice on bias and serve atop white rice. Sprinkle with sesame seeds and or grilled scallions to garnish.',
-        totalCalories: 528,
-        totalMinutes: 180,
-    },
-    {
-        recipeID: 242305,
-        title: 'Lasagna',
-        category: 'Main Dish',
-        photoUrl: 'https://photos.bigoven.com/recipe/hero/kellys-lasagne.jpg',
-        ingredients: [
-            'SoySauce',
-            'Vegetable Oil',
-            'Brown Sugar',
-            'Fresh Ginger',
-        ],
-        instructions:
-            'Prepare marinade:\r\n\r\nMix soy sauce, dry sherry, vegetable oil, brown sugar and ginger. Whisk to incorporate. \r\n\r\nAdd to plastic ziptop bag with chicken breasts. Gently press to remove air, and place in a bowl in the fridge for at least 2 hours, up to 6. \r\n\r\nTurn on grill to medium heat. Oil grill. Place chicken breasts on grill, and let cook for 2 minutes. Then turn 90 degrees. Cook for 2 more minutes and flip. Cook for 2 more minutes. Turn 90 degrees. Check for doneness, remove from grill when done. Let rest on cutting board for 2 minutes. Slice on bias and serve atop white rice. Sprinkle with sesame seeds and or grilled scallions to garnish.',
-        totalCalories: 528,
-        totalMinutes: 180,
-    },
-    {
-        recipeID: 242305,
-        title: 'Lasagna',
-        category: 'Main Dish',
-        photoUrl: 'https://photos.bigoven.com/recipe/hero/kellys-lasagne.jpg',
-        ingredients: [
-            'SoySauce',
-            'Vegetable Oil',
-            'Brown Sugar',
-            'Fresh Ginger',
-        ],
-        instructions:
-            'Prepare marinade:\r\n\r\nMix soy sauce, dry sherry, vegetable oil, brown sugar and ginger. Whisk to incorporate. \r\n\r\nAdd to plastic ziptop bag with chicken breasts. Gently press to remove air, and place in a bowl in the fridge for at least 2 hours, up to 6. \r\n\r\nTurn on grill to medium heat. Oil grill. Place chicken breasts on grill, and let cook for 2 minutes. Then turn 90 degrees. Cook for 2 more minutes and flip. Cook for 2 more minutes. Turn 90 degrees. Check for doneness, remove from grill when done. Let rest on cutting board for 2 minutes. Slice on bias and serve atop white rice. Sprinkle with sesame seeds and or grilled scallions to garnish.',
-        totalCalories: 528,
-        totalMinutes: 180,
-    },
-];
+let connectionString =
+    'mongodb+srv://dbUser:s2auDokBPzzgLdgP@recipedb.6ejbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const exampleRecipe = new Recipe({
+    title: 'Salat',
+    category: 'Appetizer',
+    photoUrl: 'https://images.lecker.de/,id=4dba2952,b=lecker,w=610,cg=c.jpg',
+});
+
+// TODO not Working yet, "MongooseError: Operation `recipes.insertOne()` buffering timed out after 10000ms"
+exampleRecipe.save(function (error, document) {
+    if (error) console.error(error);
+    console.log(document);
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
