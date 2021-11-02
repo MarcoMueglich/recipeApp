250509ab0262cddab4be1e46e608fa4c957ce255
MarcoMueglich <92206527+MarcoMueglich@users.noreply.github.com>
2021-11-02 18:26:14 +0100

    Fixed merge errors

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-const Recipe = require('../models/Recipe');
 
 const recipeController = require('../controllers/recipeController');
 
