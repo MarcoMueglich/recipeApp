6cd43ab50b0d337bacb45175c2c1804fe1ad48fd
MarcoMueglich <92206527+MarcoMueglich@users.noreply.github.com>
2021-11-03 10:04:04 +0100

    Expanded Recipe Model

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,14 @@ const Schema = mongoose.Schema;
 const recipeSchema = new Schema({
     title: String,
     category: String,
-    photoUrl: String,
+    imgUrl: String,
+    instructions: String,
+    ingredients: [String],
+    amounts: [String],
+});
+
+recipeSchema.virtual('url').get(function () {
+    return '/rezepte/' + this._id;
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
