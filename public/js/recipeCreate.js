c5d4d2ca51836f673fcc6df5844bef742cdd9ba8
MarcoMueglich <92206527+MarcoMueglich@users.noreply.github.com>
2021-11-05 18:25:53 +0100

    Added Comments for Clarity

--- public/js/recipeCreate.js
+++ public/js/recipeCreate.js
@@ -1,6 +1,5 @@
 var addIngredientBtn = document.getElementById('addIngredientBtn');
 var removeIngredientBtn = document.getElementById('removeIngredientBtn');
-var lastRow = document.getElementById('lastRow');
 var ingredientCounter = 1;
 
 var addIngredientInput = () => {
@@ -9,10 +8,12 @@ var addIngredientInput = () => {
 
     var inputRows = document.getElementsByClassName('inputRow');
 
+    // Convert inputHTML to DOM Element and add after the last Input
     inputRows[inputRows.length - 1].after(
         document.createRange().createContextualFragment(inputHTML)
     );
 
+    //Move the add and remove Button to the new Row
     inputRows[inputRows.length - 1].appendChild(addIngredientBtn.parentElement);
     inputRows[inputRows.length - 1].appendChild(
         removeIngredientBtn.parentElement
@@ -20,9 +21,12 @@ var addIngredientInput = () => {
 };
 
 var removeIngredientInput = () => {
+    // Removing is only possible if there is more than 1 Input
     if (ingredientCounter > 1) {
         ingredientCounter--;
         var inputRows = document.getElementsByClassName('inputRow');
+
+        // Move the add and remove Button to Row before
         inputRows[inputRows.length - 2].appendChild(
             addIngredientBtn.parentElement
         );
@@ -30,6 +34,7 @@ var removeIngredientInput = () => {
             removeIngredientBtn.parentElement
         );
 
+        // Remove the last Input
         inputRows[inputRows.length - 1].remove();
     }
 };
