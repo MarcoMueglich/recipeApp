29314090e34e1193d4b07d3be8d19db953290b02
MarcoMueglich <92206527+MarcoMueglich@users.noreply.github.com>
2021-11-05 18:22:24 +0100

    Added Removal of Ingredients

--- public/js/recipeCreate.js
+++ public/js/recipeCreate.js
@@ -1,4 +1,5 @@
 var addIngredientBtn = document.getElementById('addIngredientBtn');
+var removeIngredientBtn = document.getElementById('removeIngredientBtn');
 var lastRow = document.getElementById('lastRow');
 var ingredientCounter = 1;
 
@@ -7,13 +8,31 @@ var addIngredientInput = () => {
     var inputHTML = `<div class="row mt-2 inputRow"><div class="col-2"><input class="form-control" id="inputIngredientAmount${ingredientCounter}" type="text"></div><div class="col-8"><input class="form-control" id="inputIngredient${ingredientCounter}" type="text"></div>`;
 
     var inputRows = document.getElementsByClassName('inputRow');
+
     inputRows[inputRows.length - 1].after(
         document.createRange().createContextualFragment(inputHTML)
     );
 
     inputRows[inputRows.length - 1].appendChild(addIngredientBtn.parentElement);
+    inputRows[inputRows.length - 1].appendChild(
+        removeIngredientBtn.parentElement
+    );
 };
 
-// TODO implement removal of Ingredients
+var removeIngredientInput = () => {
+    if (ingredientCounter > 1) {
+        ingredientCounter--;
+        var inputRows = document.getElementsByClassName('inputRow');
+        inputRows[inputRows.length - 2].appendChild(
+            addIngredientBtn.parentElement
+        );
+        inputRows[inputRows.length - 2].appendChild(
+            removeIngredientBtn.parentElement
+        );
+
+        inputRows[inputRows.length - 1].remove();
+    }
+};
 
 addIngredientBtn.addEventListener('click', addIngredientInput);
+removeIngredientBtn.addEventListener('click', removeIngredientInput);
